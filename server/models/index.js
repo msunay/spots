36326c6945dc70c3e5494667c663b1262bd3c7f7
Alex <78169857+msunay@users.noreply.github.com>
2023-08-26 18:21:43 +0100

    .env.local updated

--- server/models/index.js
+++ server/models/index.js
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
 
 main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/spots');
-  console.log('database connneted :)');
+async function main () {
+  await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`).then(() => {
+    console.log('database connneted :)');
+  })
 }
 
 const locationSchema = new mongoose.Schema({
-    type: String,
-    properties: {
-      Name: { type: String, required: true },
-      description: String,
-      gx_media_links: String
-    },
-    geometry: {
-      coordinates: [Number]
-    }
-  })
+  type: String,
+  properties: {
+    Name: { type: String, required: true },
+    description: String,
+    gx_media_links: String
+  },
+  geometry: {
+    coordinates: [Number]
+  }
+})
 
 
 const LondonLocation = mongoose.model('London', locationSchema);
